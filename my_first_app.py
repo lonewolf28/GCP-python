21e1737c19860d7d3db370bccc9b88096bfe1e71
Rah Barath <raajbarath86@gmail.com>
2017-08-29 11:39:40 -0400

    fixed string - syntax error

--- my_first_app.py
+++ my_first_app.py
@@ -3,9 +3,9 @@ import webapp2
 class HomePage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/html'
-		self.response.write('<h3>Hello World!</h3><br/><p>This is a test page</p>
+		self.response.write('<h3>Hello World!</h3><br/><p>This is a test page</p>')
 		
 
 
 app = webapp2.WSGIApplication([
-	('/'. HomePage)],debug=True)
+	('/'. HomePage),],debug=True)
